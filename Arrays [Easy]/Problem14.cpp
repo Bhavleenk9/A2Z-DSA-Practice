3eaaeff4fee0e1d8461d8b8edda887faf9948b4a
Bhavleen Kaur <110817082+Bhavleenk9@users.noreply.github.com>
2024-08-23 23:53:31 +0530

    Create Problem14.cpp

---
+++ Arrays [Easy]/Problem14.cpp
@@ -0,0 +1,84 @@
+// This is the code for Longest subarray with given sum K (positives + Negatives).
+
+// Brute force approach.
+/* #include<bits/stdc++.h>
+using namespace std;
+int subarray(int arr[], int K, int n)
+{
+    int len = 0;
+    for(int i=0; i<n; i++)
+    {
+        int sum = 0;
+        for(int j=i; j<n; j++)
+        {
+            sum += arr[j];
+            if(sum == K)
+            {
+                len = max(len, j-i+1);
+            }
+        }
+    }
+    return len;
+}
+int main()
+{
+    int n,K;
+    cout<<"Enter the size of Array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of Array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the sum whose subarray length is to be found: ";
+    cin>>K;
+    cout<<"The longest subarray with sum "<<K<<" in this Array is: "<<subarray(arr,K,n);
+    return 0;
+} */
+
+// Optimal approach.
+#include<bits/stdc++.h>
+using namespace std;
+int subarray(int arr[], int K, int n)
+{
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for(int i=0; i<n; i++)
+    {
+        sum += arr[i];
+        if(sum == K)
+        {
+            maxLen = max(maxLen, i+1);
+        }
+        int rem = sum - K;
+        if(preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if(preSumMap.find(sum) == preSumMap.end())
+        {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
+int main()
+{
+    int n,K;
+    cout<<"Enter the size of Array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of Array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the sum whose subarray length is to be found: ";
+    cin>>K;
+    cout<<"The longest subarray with sum "<<K<<" in this Array is: "<<subarray(arr,K,n);
+    return 0;
+} 
+
